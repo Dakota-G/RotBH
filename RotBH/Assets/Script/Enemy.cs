a039a0dada11bc53c2bccdf2e1142b751ff7e12a
Bryanna Savelesky <bryanna.savelesky@gmail.com>
2020-10-12 20:36:17 -0700

    Add enemy wander and low health flee

--- RotBH/Assets/Script/Enemy.cs
+++ RotBH/Assets/Script/Enemy.cs
@@ -9,32 +9,81 @@ public class Enemy : MonoBehaviour
     
     [SerializeField]    
     private int MaxHP;
+    public Healthbar HPbar;
+
+    // Random Move attributes
+    private float latestWanderChangeTime;
+    private float moveChangeTime;
+    private bool wandering;
+    private Vector2 wanderDirection;
+    private Vector2 movementPerSecond;
+
+    // Follow attributes
     private float speed;
 
     private Transform target;
 
     public Transform Target { get => target; set => target = value; }
     public float Speed { get => speed; set => speed = value; }
-    public Healthbar HPbar;
 
     void Start()
     {
         HP = MaxHP;
         HPbar.SetHealth(HP, MaxHP);
+        speed = 0.5f;
+        latestWanderChangeTime = 0f;
+        wandering = true;
+        moveChangeTime = Random.Range(2f,4f);
+        calculateRandomMove();
     }
 
     private void FollowTarget()
     {
-        if(target != null)
+        int direction = 1;
+        if((float)HP/MaxHP <= 0.2)
         {
-            Debug.Log("Target found");
-            transform.position = Vector2.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);
-        }
+            direction = -1;
+        }       
+        transform.position = Vector2.MoveTowards(transform.position, target.position, direction * Speed * Time.deltaTime);
+    }
+
+     void calculateRandomMove(){
+        wanderDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+        movementPerSecond = wanderDirection * speed;
     }
 
     protected void Update()
     {
-        FollowTarget();
+        if(target != null)
+        {
+            FollowTarget();
+        }
+        else
+        {
+            if (Time.time - latestWanderChangeTime > moveChangeTime && wandering)
+            {
+                speed = 0;
+                wandering = false;
+                latestWanderChangeTime = Time.time;
+                //Time until enemy begins moving again
+                moveChangeTime = Random.Range(1f,2f);
+            }
+            else if(Time.time - latestWanderChangeTime > moveChangeTime && !wandering)
+            {
+                speed = 0.5f;
+                latestWanderChangeTime = Time.time;
+                calculateRandomMove();
+                wandering = true;
+                //Time until enemy stops again
+                moveChangeTime = Random.Range(2f,4f);
+            }  
+            if(speed > 0)
+            {
+                transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime), 
+                transform.position.y + (movementPerSecond.y * Time.deltaTime));
+            }
+
+        }
     }
 
     public void TakeDamage(int damage)
