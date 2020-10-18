1c8dba3170ac19962f8c8a530fbdc3dd1050ddbf
Dakota-G <dakota.giltner@gmail.com>
2020-10-17 18:59:58 -0700

    Add(Sprint Logic)
    Pressing Left Shift now allows the player to sprint

--- RotBH/Assets/Script/PC_Class.cs
+++ RotBH/Assets/Script/PC_Class.cs
@@ -9,7 +9,8 @@ public class PC_Class : MonoBehaviour
     public int MaxHP = 100;
     public int MP = 10;
     public int InventorySize = 5;
-    public float speed = 5f;
+    public float speed = 3f;
+    private float moveSpeed;
     public bool IsAlive = true;
     public List<Potion> hp_pots;
     public List<Potion> mp_pots;
@@ -28,32 +29,30 @@ public class PC_Class : MonoBehaviour
     public Camera cam;
     Vector2 movement;
     Vector2 mousePos;
-    void Move_Update()
+
+    void Move()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-    }
-    void Look_Update()
-    {
-         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
 
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
     }
+    
     public Transform firePoint;
     public GameObject bulletPrefab;
     public float bulletForce = 1f;
     void Shoot()
     {
         if(Input.GetButtonDown("Fire1"))
-    {    
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
-    }
+        {    
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        }
     }
     void Drink_Potion()
     {
@@ -131,13 +130,6 @@ public class PC_Class : MonoBehaviour
 
     void Die()
     {
-        // Until we have a death condition, here's a line to test dying
-        if(Input.GetKeyDown(KeyCode.F))
-        {
-            IsAlive = false;
-            ThrowAllPotions();
-            Destroy(gameObject);
-        }
         ThrowAllPotions();
         Destroy(gameObject);
     }
@@ -148,10 +140,13 @@ public class PC_Class : MonoBehaviour
     }
     void Update()
     {
-        Move_Update();
-        Look_Update();
         Drink_Potion();
         Shoot();
+        moveSpeed = Input.GetKey(KeyCode.LeftShift) ? speed*2 : speed;
+    }
+    void FixedUpdate()
+    {
+        Move();
     }
 
 }
