1e85ee8296dec2092682611f183a391d32781be6
Dakota-G <dakota.giltner@gmail.com>
2020-10-20 21:31:30 -0700

    Create(RandomSpawn.cs; SlimePrefab)
    Created a script that spawns a randomly placed slime every five seconds. It pulls the slime from an array that can increase the amount of enemies that spawn at a time.
    RandomSpawn is attached to the main camera.

---
+++ RotBH/Assets/Script/RandomSpawn.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public bool spawn;
+    public int spawnReset;
+    public Vector3 spawnPosition;
+    public float spawnTime = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // spawn = true;
+        // spawnReset = 0;
+        InvokeRepeating("SpawnRandom", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(spawn == true)
+        // {
+        //     SpawnRandom();
+        // }
+        // else
+        // {
+        //     spawnReset += 1;
+        //     if(spawnReset >= 10000)
+        //     {
+        //         spawn = true;
+        //     }
+        // }
+    }
+    void SpawnRandom()
+    {
+        foreach(GameObject enemy in enemies)
+        {
+            spawnPosition.x = Random.Range(-5,5);
+            spawnPosition.y = Random.Range(-5,5);
+            spawnPosition.z = 0;
+            Instantiate(enemy, spawnPosition, Quaternion.identity);
+        }
+        spawn = false;
+        spawnReset = 0;
+    }
+}
